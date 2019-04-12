5cf4c82b50c894928d40e17413df7b26f5dd0a47
Armando Barron Garcia <madaraushiha54@gmail.com>
2019-04-12 00:12:37 -0500

    Shared Preferences terminado

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_practica/DashBoard.Dart';
 import 'package:flutter_practica/Login.Dart';
 import 'package:splashscreen/splashscreen.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() => runApp(
     MaterialApp(
@@ -18,15 +22,72 @@ class SplashScreenIni extends StatefulWidget
   }
 
 }
+bool checkValue=true;
+SharedPreferences sharedPreferences;
+
+
+
 
 class SplashScreenState extends State<SplashScreenIni>
 {
+
+  @override
+  void initState() {
+    Verificar();
+    super.initState();
+  }
+
+  Verificar() async
+  {
+    sharedPreferences = await SharedPreferences.getInstance();
+
+    if(sharedPreferences.getString("username")=="")
+      {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (BuildContext context)=>new Login())
+
+        );
+
+      }
+      else
+        {
+          getCredential();
+        }
+
+  }
+  getCredential() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+    final user = sharedPreferences.getString("username");
+    final pass = sharedPreferences.getString("password");
+    http.Response response = await http.get(
+        Uri.encodeFull("http://192.168.1.2:5000/ws/login/${user}/${pass}"),
+        headers: { "Accept": "application/json"}
+    );
+
+    print(response.body);
+    if(response.statusCode==200)
+    {
+     // DashBoard();
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (BuildContext context)=>new DashBoard())
+
+    );
+    }
+  }
+
+
+
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new SplashScreen(
       seconds: 10,
-      navigateAfterSeconds: Login(),
+      //navigateAfterSeconds: Verificar(),
       //navigateAfterSeconds: DashBoard(),
       title: Text("Mi Aplicacion :)",
        style: TextStyle(
